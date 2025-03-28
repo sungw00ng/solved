3431b081e346c0f3f3627c1b6b5d0734ae65e3eb
Sungwoong Park <42471370+sungw00ng@users.noreply.github.com>
2025-03-28 14:47:53 +0900

    Create 73.3점.py

---
+++ 오답노트/숫자짝꿍/73.3점.py
@@ -0,0 +1,19 @@
+def solution(X, Y):
+    answer = []
+    
+    for i in X:
+        if i in Y:  
+            answer.append(i)
+            Y = Y.replace(i, "", 1)  
+    
+    #일치안하는 경우
+    if not answer:  
+        return "-1"
+    
+    # 하나의 0 반환
+    if all(i == '0' for i in answer):
+        return "0"
+
+    # 내림차순으로 정렬
+    answer = sorted(answer, reverse=True)
+    return ''.join(answer)
