7f4c8f9d9192de4f010f4b732eac983af75d9108
Sungwoong Park <42471370+sungw00ng@users.noreply.github.com>
2025-03-28 14:31:45 +0900

    Update 숫자 짝꿍.py

--- 프로그래머스/Lv. 1/숫자 짝꿍.py
+++ 프로그래머스/Lv. 1/숫자 짝꿍.py
@@ -1,28 +1,19 @@
 def solution(X, Y):
-    X = str(X)
-    Y = str(Y)
     answer = []
     
-    #작은 문자열 찾기
-    small, large = (X, Y) if len(X) <= len(Y) else (Y, X)
-    
-    for i in small:
-        if i in large:
+    for i in X:
+        if i in Y:  
             answer.append(i)
-            # 한 번만 제거
-            large = large.replace(i, "", 1)  
-            """
-            break쓰면 될 것 같지만 .. ? 반례가 존재했음.
-            """
-
-    if not answer:
+            Y = Y.replace(i, "", 1)  
+    
+    #일치안하는 경우
+    if not answer:  
         return "-1"
-
-    #내림차순
-    answer = sorted(answer, reverse=True)
     
     # 하나의 0 반환
-    if answer[0] == "0":
+    if all(i == '0' for i in answer):
         return "0"
-    
+
+    # 내림차순으로 정렬
+    answer = sorted(answer, reverse=True)
     return ''.join(answer)
