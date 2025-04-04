e06c1565d3ab5253b1d561a1cfb7f6535771e178
Sungwoong Park <42471370+sungw00ng@users.noreply.github.com>
2025-04-04 10:22:59 +0900

    Create 문자열을 정수로 바꾸기.py

---
+++ 프로그래머스/Lv. 1/문자열을 정수로 바꾸기.py
@@ -0,0 +1,9 @@
+def solution(s):
+    answer= 0
+    if s[0]=='+':
+        answer += int(s[1:])
+    elif s[0]=='-':
+        answer -= int(s[1:])
+    elif s[0].isdigit():
+        answer += int(s)
+    return answer
