f25628ddd0edf561694d0ef35f7b441ac562ff10
Sungwoong Park <42471370+sungw00ng@users.noreply.github.com>
2025-07-07 21:50:48 +0900

    Create 구명보트.py

---
+++ 프로그래머스/Lv. 2/구명보트.py
@@ -0,0 +1,10 @@
+def solution(people, limit):
+    people.sort()
+    left,right=0,len(people)-1
+    cnt=0
+    while left<=right:
+        if people[left]+people[right]<=limit:
+            left+=1  
+        right-=1     
+        cnt+=1       
+    return cnt
