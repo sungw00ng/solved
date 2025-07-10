1a3b7f9f1c7908d2636cd6afdeed831b74fbc4b1
Sungwoong Park <42471370+sungw00ng@users.noreply.github.com>
2025-07-10 16:04:07 +0900

    Update 할인 행사.py

--- 프로그래머스/Lv. 2/할인 행사.py
+++ 프로그래머스/Lv. 2/할인 행사.py
@@ -5,15 +5,24 @@ def solution(want,number,discount):
     want_dict=dict(zip(want,number))  
     max_length=len(discount)-10
     
-    # sliding window
-    for i in range(max_length+1):
-        current_window=discount[i:i+10]
-        current_counter=Counter(current_window)
+    current_window=discount[:10]
+    current_counter=Counter(current_window)  
+    
+    #true false 
+    if all(current_counter.get(food,0)==want_dict[food]  \
+    for food in want_dict):
+        day+=1
+    
+    #sliding window algo(O(N)*O(1))
+    for i in range(1,max_length+1):
+        current_counter[discount[i-1]]-=1
+        if current_counter[discount[i-1]]==0:
+            del current_counter[discount[i-1]]  
+        current_counter[discount[i+9]]+=1
         
-        # 원하는 제품과 수량이 일치하는지 확인
-        #get->있으면 w 없으면 0
+        #true false 
         if all(current_counter.get(food,0)==want_dict[food] \
         for food in want_dict):
             day+=1
-    
+
     return day
