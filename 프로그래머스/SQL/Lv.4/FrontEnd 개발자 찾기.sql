1d28158f837f19e8d2ad066f4aa2dbe597afaec3
ParkSeongung <42471370+sungw00ng@users.noreply.github.com>
2025-08-23 23:11:49 +0900

    Create FrontEnd 개발자 찾기.sql

---
+++ 프로그래머스/SQL/Lv.4/FrontEnd 개발자 찾기.sql
@@ -0,0 +1,13 @@
+SELECT 
+id,
+email,
+first_name,
+last_name
+FROM developers 
+WHERE skill_code & (
+    -- JavaScript, React, Vue
+    SELECT SUM(code)
+    FROM skillcodes
+    WHERE category='Front End'
+)
+ORDER BY id;
